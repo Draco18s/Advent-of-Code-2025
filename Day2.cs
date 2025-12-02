cc1ad1fce068a72aef15fb869aafe85359864cb9
Draco18s <draco18s@gmail.com>
2025-12-02 00:46:47 -0600

    Day 2

--- Day2.cs
+++ Day2.cs
@@ -9,24 +9,71 @@ namespace AdventofCode2025
 	{
 		internal static long Part1(string input)
 		{
-			string[] lines = input.Split('\n');
+			string[] lines = input.Split(',');
 			long result = 0l;
 			foreach (string line in lines)
 			{
-
+				string[] ranges = line.Split('-');
+				long min = long.Parse(ranges[0]);
+				long max = long.Parse(ranges[1]);
+				for (long i = min; i <= max; i++)
+				{
+					string iStr = i.ToString();
+					int l = iStr.Length;
+					if (l % 2 == 1) continue;
+					if (iStr.EndsWith(iStr.Substring(0, l / 2)))
+					{
+						result += i;
+					}
+				}
 			}
 			return result;
 		}
 
 		internal static long Part2(string input)
 		{
-			string[] lines = input.Split('\n');
+			string[] lines = input.Split(',');
 			long result = 0l;
 			foreach (string line in lines)
 			{
+				string[] ranges = line.Split('-');
+				long min = long.Parse(ranges[0]);
+				long max = long.Parse(ranges[1]);
+				for (long i = min; i <= max; i++)
+				{
+					string iStr = i.ToString();
+					int l = iStr.Length;
+					if (IsValid(iStr, l))
+					{
+						continue;
+					}
 
+					result += i;
+				}
 			}
 			return result;
+		}
+
+		private static bool IsValid(string str, int len)
+		{
+			for (int i = 1; i <= len/2; i++)
+			{
+				if ((str.Length / (float)i)%1 > 0)
+				{
+					continue;
+				}
+				string[] subStr = Split(str,i).ToArray();
+				if (subStr.All(x => x == subStr[0]))
+					return false;
+			}
+
+			return true;
+		}
+
+		static IEnumerable<string> Split(string str, int chunkSize)
+		{
+			return Enumerable.Range(0, str.Length / chunkSize)
+				.Select(i => str.Substring(i * chunkSize, chunkSize));
 		}
 	}
 }
